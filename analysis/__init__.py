5a0c864f3adfe894d858f077f23c763e643baaee
michael.graf3110 <michael.graf3110@gmail.com>
2020-07-21 07:46:57 +0200

    added package init and setup.py

---
+++ analysis/__init__.py
@@ -0,0 +1,7 @@
+from analysis.analysis.cluster import *
+from analysis.analysis.gradient_boosting import *
+from analysis.analysis.statistics import *
+from analysis.analysis.util import *
+from analysis.analysis.plots import *
+from analysis.analysis.plots import scatter_correlation_plot
+from analysis.analysis.process_data import load_data
---
+++ analysis/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="hypothesis",
+    version="0.0.1",
+    author="Michael Graf",
+    author_email="michael.graf3110@gmail.com",
+    description="tool for hypothesis discovery",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
